acfd368fe3be26c44218f4ce052c683a13b1bf84
Lucas Ginard <lucasmatiasginard@hotmail.es>
2021-06-05 20:35:56 -0400

    implement firebase ,UI fragment Info

--- app/src/main/java/com/lucasginard/dolarpy/ui/home/fragments/infoFragment.kt
+++ app/src/main/java/com/lucasginard/dolarpy/ui/home/fragments/infoFragment.kt
@@ -1,10 +1,16 @@
 package com.lucasginard.dolarpy.ui.home.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isEmpty
+import androidx.core.view.isNotEmpty
+import androidx.fragment.app.Fragment
 import com.lucasginard.dolarpy.R
 import com.lucasginard.dolarpy.databinding.FragmentInfoBinding
 
@@ -22,8 +28,44 @@ class infoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentInfoBinding.inflate(inflater, container, false)
+        configureOnClickListener()
         return _binding.root
     }
+
+    private fun configureOnClickListener(){
+        _binding.linearTextReport.setOnClickListener {
+            if (_binding.linearForm.visibility == View.GONE){
+                _binding.linearForm.visibility = View.VISIBLE
+                _binding.ivArrow.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.ic_arrow_up))
+            }else{
+                _binding.linearForm.visibility = View.GONE
+                _binding.ivArrow.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.ic_arrow_down))
+            }
+        }
+
+        _binding.btnSend.setOnClickListener {
+            if(isValid()){
+                Log.d("testValid","si")
+            }
+        }
+
+        _binding.linearGit.setOnClickListener {
+            val uri: Uri = Uri.parse("https://github.com/melizeche/dolarPy")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun isValid(): Boolean {
+        return if (_binding.tvArea.editText?.text.isNullOrEmpty()){
+            _binding.tvArea.error = "Ingrese"
+            false
+        }else{
+            true
+        }
+    }
+
 
     companion object {
         @JvmStatic
