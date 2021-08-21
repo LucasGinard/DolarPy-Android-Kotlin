10591652d5087ff22c4a7f606933861702f9076d
Lucas Ginard <lucasmatiasginard@hotmail.es>
2021-08-21 01:20:18 -0400

    update libraries  , crashlytics disable in debug

--- app/src/main/java/com/lucasginard/dolarpy/DolarApp.kt
+++ app/src/main/java/com/lucasginard/dolarpy/DolarApp.kt
@@ -2,6 +2,7 @@ package com.lucasginard.dolarpy
 
 import android.app.Application
 import androidx.room.Room
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.lucasginard.dolarpy.database.DolarDatabase
 
 class DolarApp : Application() {
@@ -13,6 +14,9 @@ class DolarApp : Application() {
     override fun onCreate() {
         super.onCreate()
         database =  Room.databaseBuilder(this, DolarDatabase::class.java, "dolar-db").build()
+        if(BuildConfig.DEBUG){
+            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false);
+        }
     }
 
 }
