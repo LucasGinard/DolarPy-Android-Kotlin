3a8747611ec0b9b0a8c070834465b625efd810ce
Lucas Ginard <lucasmatiasginard@hotmail.es>
2022-06-27 22:03:52 -0400

    refactor splash

--- app/src/main/java/com/lucasginard/dolarpy/view/home/HomeActivity.kt
+++ app/src/main/java/com/lucasginard/dolarpy/view/home/HomeActivity.kt
@@ -2,20 +2,29 @@ package com.lucasginard.dolarpy.view.home
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.lucasginard.dolarpy.R
+import com.lucasginard.dolarpy.data.apiService
 import com.lucasginard.dolarpy.utils.setBackground
 import com.lucasginard.dolarpy.databinding.ActivityHomeBinding
+import com.lucasginard.dolarpy.domain.MainRepository
 import com.lucasginard.dolarpy.utils.OnHorizontalSwipeListener
+import com.lucasginard.dolarpy.utils.Tools
 import com.lucasginard.dolarpy.utils.setAppLocale
 import com.lucasginard.dolarpy.view.home.fragments.CotizacionFragment
 import com.lucasginard.dolarpy.view.home.fragments.InfoFragment
 import com.lucasginard.dolarpy.view.home.fragments.UbicacionFragment
+import com.lucasginard.dolarpy.view.viewModel.MainViewModel
+import com.lucasginard.dolarpy.view.viewModel.MyViewModelFactory
 import java.util.*
 
 
@@ -23,10 +32,14 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var preferences: SharedPreferences
     private lateinit var bindding:ActivityHomeBinding
+    private lateinit var viewModel: MainViewModel
+
     private var idSave:Int = R.id.nav_coti
+    private val retrofitService = apiService.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         bindding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(bindding.root)
         configureUI()
@@ -59,6 +72,29 @@ class HomeActivity : AppCompatActivity() {
                     else ->{
                         this.setAppLocale("en")
                         this.recreate()
+                    }
+                }
+            }
+        }
+        viewModel = ViewModelProvider(this, MyViewModelFactory(MainRepository(retrofitService))).get(
+            MainViewModel::class.java)
+        bindding.contraintBase.setBackground()
+        val config = this.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        if (viewModel.getFlatModeTheme()){
+            val saveMode = viewModel.getMode()
+            if (saveMode != 3){
+                if (Tools.flatTheme){
+                    when(saveMode){
+                        1 ->{
+                            if (config != AppCompatDelegate.MODE_NIGHT_YES){
+                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                            }
+                        }
+                        0 ->{
+                            if (config != AppCompatDelegate.MODE_NIGHT_NO){
+                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                            }
+                        }
                     }
                 }
             }
